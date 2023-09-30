3b5792b538992a498d5a3692000c087a84f37b7a
Yiweiwangau <81563224+Yiweiwangau@users.noreply.github.com>
2023-09-30 22:57:12 +1000

    feat(.gitignore): add .commitgpt.json and .commitgpt-template to gitignore to prevent commit of local configuration files feat(auth): add sign-in and sign-up pages using @clerk/nextjs for user authentication style(root): update Home page layout and heading for better user experience feat(middleware.ts): add authMiddleware to protect routes and ignore specific ones for better security and routing control

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
 
+ 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Threads</h1>
-    </main>
+    <div>
+      <h1 className = "head-text text-left">Home</h1>
+    </div>
   )
 }
