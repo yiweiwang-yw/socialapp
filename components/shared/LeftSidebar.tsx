a8f3e8369ddd0a70f5b7d155736c0889086832cc
Yiweiwangau <81563224+Yiweiwangau@users.noreply.github.com>
2023-10-01 17:06:52 +1100

    Enhance app layout and UI components
    - Revise metadata in root layout for "Social Media App".
    - Extend LeftSidebar with navigation links and Next.js Image usage.
    - Overhaul Topbar, adding logo, navigation, SignOutButton, and OrganizationSwitcher.
    - Introduce constants for sidebarLinks, profileTabs, and communityTabs.

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,5 +1,37 @@
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+
 function LeftSidebar() {
-    return <h1>LeftSidebar</h1>
+    return (
+        <section className="custom-scrollbar leftsidebar">
+            <div className="flex w-full flex-1 flex-col gap-6 px-6">
+                {sidebarLinks.map((link) => {
+                
+
+
+
+                return (
+                    <Link
+                        href={link.route}
+                        key={link.label}
+                        className="leftsidebar_link"
+                    >
+                        <Image
+                            src={link.imgURL}
+                            alt={link.label}
+                            width={24}
+                            height={24}
+                        />
+                        <p className="text-light-1 max-lg:hidden">
+                            {link.label}
+                        </p>
+                    </Link>
+                )})}
+            </div>
+        </section>
+    );
 }
 
 export default LeftSidebar;
