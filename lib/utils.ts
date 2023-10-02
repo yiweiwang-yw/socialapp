5178b76ad017d5b1a5366b10a49254fd99268733
Yiweiwang <yiweiwangau@gmail.com>
2023-10-02 20:43:52 +1100

    feat: Integrate Uploadthing for AccountProfile and extend utility functions
    - Add core Uploadthing configurations with user authentication in `uploadthing/core.ts`.
    - Implement Next.js route handlers for Uploadthing in `uploadthing/route.ts`.
    - Enhance `AccountProfile.tsx` form with image uploading capabilities using Uploadthing.
    - Generate React helpers for Uploadthing in `lib/uploadthing.ts`.
    - Add utility functions in `lib/utils.ts` for:
      - Base64 image validation.
      - Formatting date strings.
      - Formatting post count

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,39 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function isBase64Image(imageData: string) {
+  const base64Regex = /^data:image\/(png|jpe?g|gif|webp);base64,/;
+  return base64Regex.test(imageData);
+}
+
+
+export function formatDateString(dateString: string) {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+
+  const date = new Date(dateString);
+  const formattedDate = date.toLocaleDateString(undefined, options);
+
+  const time = date.toLocaleTimeString([], {
+    hour: "numeric",
+    minute: "2-digit",
+  });
+
+  return `${time} - ${formattedDate}`;
+}
+
+
+export function formatPostsCount(count: number): string {
+  if (count === 0) {
+    return "No Posts";
+  } else {
+    const postCount = count.toString().padStart(2, "0");
+    const postWord = count === 1 ? "Thread" : "Threads";
+    return `${postCount} ${postWord}`;
+  }
+}
